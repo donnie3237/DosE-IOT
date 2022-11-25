766e58ce96d01932fc7bcf139a386107066acc98
SoRa <paradorn3237@gmail.com>
2022-11-25 17:27:43 +0700

    .

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import LoginPage from './components/LoginPage/LoginPage.tsx';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginPage/>
+      <h1>hello</h1>
+    </div>
+  );
+}
+
+export default App;
