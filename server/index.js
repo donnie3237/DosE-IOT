a356ea635865565722d2da69a0bbf79ac0e7f696
SoRa <paradorn3237@gmail.com>
2022-12-07 14:01:00 +0700

    .

--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const port = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080
+const path = require('path')
 const DB_URi = process.env.DATABASE
 const mongoClient =require('mongodb').MongoClient;
 var db
@@ -15,26 +16,19 @@ app.use(express.json)
 app.use(express.urlencoded({extended:true}))
 
 //listen port and connect database
-app.listen(8080
-//     ()=>{
-//     mongoClient.connect(DB_URi,{useNewUrlparser:true},(err,result)=>{
-//     if(err){
-//         console.log("cannot connect to database")
-//     }
-//     db = result.db('IOT-database').collection('User-device')
-//     })
-//     console.log('connected to database!!')
-// })
-)
+app.listen(PORT,()=>{
+    console.log(`runnibg on port ${PORT}`)
+}
+    // ,()=>{
+    // mongoClient.connect(DB_URi,{useNewUrlparser:true},(err,result)=>{
+    // if(err){
+    //     console.log("cannot connect to database")
+    // }
+    // db = result.db('IOT-database').collection('User-device')
+    // })
+    // console.log('connected to database!!')
+    )
 
-app.get("/",(req,res)=>{
-    res.send("im from port 8080")
-})
-app.post("/user",()=>{
-    const user = {
-        "state" : req.body.state
-    }
-    db.insertOne(user, (err,res)=>{
-        console.log("insert is completed")
-    })
-})
+// app.get("/",(req,res)=>{
+//     res.sendFile(path.join(__dirname,'./main/server.html'))
+// })
